2e372eb6c15fd2d118e8eab9a23bd4ea35f9ee28
Sandeep Kadyan <sandeep.kadyan@gmail.com>
2017-09-27 01:07:47 +0100

    Create autoconfig to configure the  AWS Simple Mail sender

--- src/main/java/com/example/awssestest/AwsSesTestApplication.java
+++ src/main/java/com/example/awssestest/AwsSesTestApplication.java
@@ -3,17 +3,39 @@ package com.example.awssestest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
 
 @SpringBootApplication
 public class AwsSesTestApplication {
 
-	private final MailSender mailSender;
+    private final MailSender mailSender;
 
-	@Autowired public AwsSesTestApplication(MailSender mailSender) {
-		this.mailSender = mailSender;
-	}
-	public static void main(String[] args) {
-		SpringApplication.run(AwsSesTestApplication.class, args);
-	}
+    @Autowired
+    public AwsSesTestApplication(MailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(AwsSesTestApplication.class, args);
+        try {
+            doSendEmailWith(context);
+        } finally {
+            context.close();
+        }
+    }
+
+    private static void doSendEmailWith(ApplicationContext context) {
+        MailSender sender = context.getBean(AwsSesTestApplication.class).mailSender;
+
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("<YOUR EMAIL>@gmail.com");
+        message.setTo("<YOUR EMAIL>@gmail.com");
+        message.setSubject("Hello SES");
+        message.setText("Body text - Java");
+        sender.send(message);
+        System.out.println("Message Sent");
+    }
 }
