8dbabc29f4b8376efd52b7adcbd9604a20270a96
sevketemingurbuz <sevketemingurbuz6@gmail.com>
2025-10-12 14:47:38 +0300

    cors localhost:3000

--- src/main/java/com/example/awssestest/EmailController.java
+++ src/main/java/com/example/awssestest/EmailController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = {"https://www.mersinarslannakliyat.com", "localhost:8080"})
+@CrossOrigin(origins = {"https://www.mersinarslannakliyat.com", "https://localhost:3000"})
 @RestController
 @RequestMapping("/api")
 public class EmailController {
